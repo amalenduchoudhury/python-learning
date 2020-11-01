922ba0ef2c7a4f6f262b27c91533a11de53862a6
Choudhury <amchoudhury@deloitte.com>
2020-11-01 14:12:47 +0530

    Second Commit

--- Hello World.py
+++ Hello World.py
@@ -3,8 +3,6 @@ import sys
 print(sys.version)
 print(sys.executable)
 
-who_we_greet = input("Your Name? ")
 def greet(who_we_greet):
     greeting = 'Hello, {}'.format(who_we_greet)
     return greeting
-greet(who_we_greet)
